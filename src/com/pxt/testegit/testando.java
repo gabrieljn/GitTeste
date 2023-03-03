a695ddfed1e30d5f1e64e35a934ee5d23e4fa3b6
Vitor <vsbreturn@gmail.com>
2023-03-03 09:36:16 -0300

    teste3

--- src/com/pxt/testegit/testando.java
+++ src/com/pxt/testegit/testando.java
@@ -4,5 +4,6 @@ public class testando {
 	public static void main(String...args) {
 		System.out.println("TESTANDO GIT");
 		System.out.println("TESTE 2");
+		System.out.println("Teste 3...");
 	}
 }
